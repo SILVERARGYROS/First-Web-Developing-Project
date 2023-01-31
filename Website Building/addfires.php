c1d0757672ceeeb5c76568bd9163ba5eb3c2eb20
SILVERARGYROS <76134085+SILVERARGYROS@users.noreply.github.com>
2023-01-31 16:22:58 +0200

    Add Row Update
    - Added relation functionality in every Add file.

--- Website Building/addfires.php
+++ Website Building/addfires.php
@@ -45,7 +45,9 @@
             πλήθος_οχημάτων:
             <input type="number" name="πλήθος_οχημάτων"></input><br>
             πλήθος_εναέριων_μέσων:
-            <input type="number" name="πλήθος_εναέριων_μέσων"></input><br>
+            <input type="number" name="πλήθος_εναέριων_μέσων"></input><br><br>
+            Δήμος (συσχέτιση):
+            <input type="number" name="Δήμος"></input><br>
 
             <button type="reset" value="reset" name="resetfields">Καθαρισμός Πεδίων</button>
             <input type="submit" value="Υποβολή" name="submit"></input>
@@ -63,7 +65,8 @@
                 $a7 = $_GET['πλήθος_προσωπικού'];
                 $a8 = $_GET['πλήθος_οχημάτων'];
                 $a9 = $_GET['πλήθος_εναέριων_μέσων'];
-
+                $a10 = $_GET['Δήμος'];
+                
                 if($a1 && $a2 && $a3 && $a4 && $a5 && $a6 && $a7 && $a8 && $a9) {  
                     
                     $link = pg_connect("host=$host dbname=$db user=$user password=$pass")
@@ -71,25 +74,88 @@
 
                     $result = pg_query($link, "SELECT * FROM Δασικές_Πυρκαγιές WHERE όνομα_πυρ_σώματος='$a1' AND ημερομ_έναρξης='$a2' AND ώρα_έναρξης='$a3' 
                     AND ημερομ_κατασβ='$a4' AND ώρα_κατασβ='$a5' AND καμμένη_έκταση=$a6 AND πλήθος_προσωπικού=$a7 AND πλήθος_οχημάτων=$a8 AND πλήθος_εναέριων_μέσων=$a9;");
-                    
                     $rows = pg_num_rows($result);
-
                     if($rows>0)
                     {
-                        echo "Το στοιχείο που επιχειρήσατε να προσθέσετε υπάρχει ήδη στη βάση!";
-                    }
-                    else{
-                        $query = "INSERT INTO Δασικές_Πυρκαγιές(id, όνομα_πυρ_σώματος, ημερομ_έναρξης, ώρα_έναρξης, ημερομ_κατασβ, ώρα_κατασβ, καμμένη_έκταση, πλήθος_προσωπικού, πλήθος_οχημάτων, πλήθος_εναέριων_μέσων) VALUES (default,'$a1','$a2','$a3','$a4','$a5',$a6,$a7,$a8,$a9);";
-                        
-                        $result = pg_query($link, $query) or die("Αποτυχία προσθήκης στοιχείου!"); 
-                        echo "Το στοιχείο προστέθηκε επιτυχώς.";
-                    }
+                        if($a10)
+                        {
+                            //finding fireID
+                            $row = pg_fetch_array($result, 0);
+                            $fireID = $row["id"];
 
+                            //checking if Location exists
+                            $result = pg_query($link, "SELECT * FROM Δήμοι WHERE όνομα_Δήμου = '$a10';");
+                            $rows = pg_num_rows($result);
+                            if($rows>0) 
+                            {
+                                //finding locationID
+                                $row = pg_fetch_array($result, 0);
+                                $locationID = $row["id"];
+
+                                //checking if relation exists
+                                $result = pg_query($link, "SELECT * FROM ΕΚΔΗΛΩΘΗΚΑΝ WHERE idΔΠ = $fireID AND idΔήμοι = $locationID;");
+                                $rows = pg_num_rows($result);
+                                if($rows > 0)
+                                {
+                                    echo "Η συσχέτιση που επιχειρήσατε να εισάγετε υπάρχει ήδη!";
+                                } 
+                                else
+                                {
+                                    //inserting relation
+                                    $result = pg_query($link, "INSERT INTO ΕΚΔΗΛΩΘΗΚΑΝ (idΔΠ, idΔήμοι) VALUES ($fireID, $locationID);");
+                                    echo "Η συσχέτιση προστέθηκε επιτυχώς.";
+                                }
+                            }
+                            else
+                            {
+                                echo "Ο Δήμος με τον οποίο επιχειρήσατε να κάνετε συσχέτιση δεν υπάρχει!\nΠαρακαλώ εισάγετε πρώτα τον Δήμο αυτό.";
+                            }
+                        }
+                        else
+                        {
+                            echo "Το στοιχείο που επιχειρήσατε να προσθέσετε υπάρχει ήδη στη βάση!";
+                        }
+                    }
+                    else
+                    {
+                        if($a10)
+                        {
+                            //checking if Location exists
+                            $result = pg_query($link, "SELECT * FROM Δήμοι WHERE όνομα_Δήμου = '$a10';") or die("Αυτός ο Δήμος δεν είναι διαθέσιμος!");
+                            $rows = pg_num_rows($result);
+                            if($rows>0) 
+                            {
+                                //finding locationID
+                                $row = pg_fetch_array($result, 0);
+                                $locationID = $row["id"];
+
+                                //inserting fire
+                                $query = "INSERT INTO Δασικές_Πυρκαγιές(id, όνομα_πυρ_σώματος, ημερομ_έναρξης, ώρα_έναρξης, ημερομ_κατασβ, ώρα_κατασβ, καμμένη_έκταση, πλήθος_προσωπικού, πλήθος_οχημάτων, πλήθος_εναέριων_μέσων) VALUES (default,'$a1','$a2','$a3','$a4','$a5',$a6,$a7,$a8,$a9);";
+                                $result = pg_query($link, $query) or die("Αποτυχία προσθήκης στοιχείου!"); 
+
+                                //finding fireID
+                                $result = pg_query($link, "SELECT * FROM Δασικές_Πυρκαγιές WHERE όνομα_πυρ_σώματος='$a1' AND ημερομ_έναρξης='$a2' AND ώρα_έναρξης='$a3' 
+                                AND ημερομ_κατασβ='$a4' AND ώρα_κατασβ='$a5' AND καμμένη_έκταση=$a6 AND πλήθος_προσωπικού=$a7 AND πλήθος_οχημάτων=$a8 AND πλήθος_εναέριων_μέσων=$a9;");
+                                $row = pg_fetch_array($result, 0);
+                                $fireID = $row["id"];
+
+                                //inserting relation
+                                $result = pg_query($link, "INSERT INTO ΕΚΔΗΛΩΘΗΚΑΝ (idΔΠ, idΔήμοι) VALUES ($fireID, $locationID);");
+                                echo "Το στοιχείο και η συσχέτισή του προστέθηκαν επιτυχώς.";
+                            }
+                        }
+                        else
+                        {
+                            $query = "INSERT INTO Δασικές_Πυρκαγιές(id, όνομα_πυρ_σώματος, ημερομ_έναρξης, ώρα_έναρξης, ημερομ_κατασβ, ώρα_κατασβ, καμμένη_έκταση, πλήθος_προσωπικού, πλήθος_οχημάτων, πλήθος_εναέριων_μέσων) VALUES (default,'$a1','$a2','$a3','$a4','$a5',$a6,$a7,$a8,$a9);";
+                            $result = pg_query($link, $query) or die("Αποτυχία προσθήκης στοιχείου!"); 
+                            echo "Το στοιχείο προστέθηκε επιτυχώς.";
+                        }
+                    }
                     pg_close($link);
                 }
                 else
                 {
-                    echo "Παρακαλώ συμπληρώστε όλα τα πεδία!\n";
+                    echo "Παρακαλώ συμπληρώστε όλα τα απαραίτητα πεδία!\n";
                 }
             }      
             clearstatcache();
--- Website Building/addlocations.php
+++ Website Building/addlocations.php
@@ -37,7 +37,9 @@
             γεωγ_πλάτος:
             <input type="number" step=any name="γεωγ_πλάτος"></input><br>
             γεωγ_μήκος:
-            <input type="number" step=any name="γεωγ_μήκος"></input><br>
+            <input type="number" step=any name="γεωγ_μήκος"></input><br><br>
+            Μετεορολογικός Σταθμός (συσχέτιση):
+            <input type="text" step=any name="Σταθμός"></input><br>
 
             <button type="reset" value="reset" name="resetfields">Καθαρισμός Πεδίων</button>
             <input type="submit" value="Υποβολή" name="submit"></input>
@@ -51,33 +53,60 @@
                 $a3 = $_GET['όνομα_Δήμου'];
                 $a4 = $_GET['γεωγ_πλάτος'];
                 $a5 = $_GET['γεωγ_μήκος'];
+                $a6 = $_GET['Σταθμός'];
 
                 if($a1 && $a2 && $a3 && $a4 && $a5) {  
 
                     $link = pg_connect("host=$host dbname=$db user=$user password=$pass")
-                        or die ("Αποτυχία Σύνδεσης!");
+                    or die ("Αποτυχία Σύνδεσης!");
 
                     $result = pg_query($link, "SELECT * FROM Δήμοι WHERE όνομα_περιφέριας='$a1' AND όνομα_νομού='$a2' AND όνομα_Δήμου='$a3' 
                     AND γεωγ_πλάτος=$a4 AND γεωγ_μήκος=$a5;");
-                    
                     $rows = pg_num_rows($result);
-
                     if($rows>0)
                     {
                         echo "Το στοιχείο που επιχειρήσατε να προσθέσετε υπάρχει ήδη στη βάση!";
                     }
-                    else{
-                        $query = "INSERT INTO Δήμοι(id, όνομα_περιφέριας, όνομα_νομού, όνομα_Δήμου, γεωγ_πλάτος, γεωγ_μήκος) VALUES (default,'$a1','$a2','$a3',$a4,$a5);";
-                        
-                        $result = pg_query($link, $query) or die("Αποτυχία προσθήκης στοιχείου!"); 
-                        echo "Το στοιχείο προστέθηκε επιτυχώς.";
-                    }
+                    else
+                    {
+                        if($a6)
+                        {
+                            //checking if station exists
+                            $result = pg_query($link, "SELECT * FROM Μ_Σταθμοί WHERE όνομα_μετεωρ_σταθμού = '$a6';") or die("Αυτός ο Σταθμός δεν είναι διαθέσιμος!");
+                            $rows = pg_num_rows($result);
+                            if($rows>0) 
+                            {
+                                //finding stationID
+                                $row = pg_fetch_array($result, 0);
+                                $stationID = $row["id"];
 
+                                //inserting location
+                                $query = "INSERT INTO Δήμοι(id, όνομα_περιφέριας, όνομα_νομού, όνομα_Δήμου, γεωγ_πλάτος, γεωγ_μήκος) VALUES (default,'$a1','$a2','$a3',$a4,$a5);";
+                                $result = pg_query($link, $query) or die("Αποτυχία προσθήκης στοιχείου!"); 
+
+                                //finding locationID
+                                $result = pg_query($link, "SELECT * FROM Δήμοι WHERE όνομα_περιφέριας='$a1' AND όνομα_νομού='$a2' AND όνομα_Δήμου='$a3' 
+                                AND γεωγ_πλάτος=$a4 AND γεωγ_μήκος=$a5;");
+                                $row = pg_fetch_array($result, 0);
+                                $locationID = $row["id"];
+
+                                //inserting relation
+                                $result = pg_query($link, "INSERT INTO ΣΤΑΘΜΟΣ_ΑΝΑΦΟΡΑΣ (idΜΣ, idΔήμοι) VALUES ($stationID, $locationID);");
+                                echo "Το στοιχείο και η συσχέτισή του προστέθηκαν επιτυχώς.";
+                            }
+                        }
+                        else
+                        {
+                            $query = "INSERT INTO Δήμοι(id, όνομα_περιφέριας, όνομα_νομού, όνομα_Δήμου, γεωγ_πλάτος, γεωγ_μήκος) VALUES (default,'$a1','$a2','$a3',$a4,$a5);";
+                            $result = pg_query($link, $query) or die("Αποτυχία προσθήκης στοιχείου!"); 
+                            echo "Το στοιχείο προστέθηκε επιτυχώς.";
+                        }
+                    }
                     pg_close($link);
                 }
                 else
                 {
-                    echo "Παρακαλώ συμπληρώστε όλα τα πεδία!\n";
+                    echo "Παρακαλώ συμπληρώστε όλα τα απαραίτητα πεδία!\n";
                 }
             }      
             clearstatcache();
